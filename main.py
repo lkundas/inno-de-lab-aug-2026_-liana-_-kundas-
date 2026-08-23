0cceb612fe46fda94b184aea38d6945b104ff018
Лиана Кундас <lianachka609021@gmail.com>
2026-08-23 17:11:14 +0300

    Добавлено задание 5: игра угадай число

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-number = int(input("Введите целое число: "))
+import random
 
-if number % 2 == 0:
-    print(f"Число {number} — чётное.")
-else:
-    print(f"Число {number} — нечётное.")
+secret_number = random.randint(1, 20)
+attempts = 5
+
+print("Я загадал число от 1 до 20. У тебя 5 попыток!")
+
+while attempts > 0:
+    guess = int(input("Введите число: "))
+
+    if guess == secret_number:
+        print("Ты угадала! Отличная работа.")
+        break
+    elif guess < secret_number:
+        attempts -= 1
+        print(f"Слишком мало! Осталось попыток: {attempts}")
+    else:
+        attempts -= 1
+        print(f"Слишком много! Осталось попыток: {attempts}")
+
+if attempts == 0 and guess != secret_number:
+    print(f"Попытки закончились. Я загадал число: {secret_number}")
