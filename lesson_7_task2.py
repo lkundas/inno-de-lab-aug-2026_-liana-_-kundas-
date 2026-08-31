3eb4c0171164d5aa59adc8b13d9670dae1c08737
lkundas <lianachka609021@gmail.com>
2026-08-31 16:54:36 +0300

    Update lesson_7_task2.py

--- lesson_7_task2.py
+++ lesson_7_task2.py
@@ -10,15 +10,11 @@ raw_transactions = [
 
 # Реализация фильтрации в одну строку с помощью List Comprehension
 cleaned_transactions = [
-    # 4. Преобразует корректные суммы в целочисленный тип данных (int)
+    # 1. Преобразует корректные суммы в целочисленный тип данных (int)
     # 2. Извлекает числовое значение суммы платежа
-    int(tx.split(":")[1])
-    for tx in raw_transactions
-    # 1. Отсеивает все транзакции, не имеющие статус SUCCESS
-    if tx.startswith("SUCCESS")
-    # 3. Исключает аномальные транзакции с неположительной суммой (меньше или равной нулю)
-    and int(tx.split(":")[1]) > 0
-]
+    int(tx.split(":")[1]) for tx in raw_transactions
+    # 3. Отсеивает все транзакции, не имеющие статус SUCCESS и меньше или равные нулю
+    if "SUCCES" in tx and int(tx.split(":")[1]) > 0 ]
 
 # Выводим результат
 print("Очищенные транзакции:", cleaned_transactions)
